3537ec9ca90c3f2deeba47c83606c9b766303a8e
Pietro Spitella <pietrospitella@gmail.com>
2024-07-04 20:20:08 +0200

    installed main pkgs, first scaffolding

---
+++ app.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <NuxtRouteAnnouncer />
+    <Navbar/>
+    <Hero/>
+    <About/>
+    <Skills/>
+    <Footer/>
+  </div>
+</template>
+
+<script setup lang="ts">
+useHead({
+  title: 'Pietro Spitella',
+  meta: [
+    { name: 'description', content: 'My amazing site.' }
+  ],
+  bodyAttrs: {
+    class: 'test'
+  },
+  script: [ { innerHTML: 'console.log(\'Hello world\')' } ]
+})
+</script>
