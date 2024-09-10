a33e85e46e8de8a901296ba98fd28d2a8b0d4df5
Pietro Spitella <pietrospitella@gmail.com>
2024-09-10 16:00:48 +0200

    ssr

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  ssr: false, 
+  ssr: true, 
   postcss: {
     plugins: {
       tailwindcss: {},
